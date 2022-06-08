ebb251efaf45e3833b718071836a8d481abea62f
Mark Kariuki <94464185+markxkamau@users.noreply.github.com>
2022-06-08 19:46:48 +0300

    Update db.php

--- Config/db.php
+++ Config/db.php
@@ -1,3 +1,7 @@
+<!-- For username, password and dbname, input the data you wish to work with  -->
+<!-- username is the database created username(s) -->
+<!-- password, input the password declared for the specific username -->
+<!-- dbname, input name of the database you would wish to work with -->
 <?php
 $hostname = "localhost";
 $username = "root";
